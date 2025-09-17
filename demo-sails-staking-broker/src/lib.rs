64c5e5093142880c3df57c7e4b393b03a824e4d4
David-HernandezM <dh29550@gmail.com>
2025-09-16 20:59:30 -0600

    sails-staking-broker updated for tests

--- demo-sails-staking-broker/src/lib.rs
+++ demo-sails-staking-broker/src/lib.rs
@@ -1,14 +1,31 @@
-#![no_std]
+// #![no_std]
 
-#[cfg(target_arch = "wasm32")]
-pub use contract_app::wasm::*;
+// #[cfg(target_arch = "wasm32")]
+// pub use contract_app::wasm::*;
 
-#[cfg(feature = "wasm-binary")]
-#[cfg(not(target_arch = "wasm32"))]
-pub use code::WASM_BINARY_OPT as WASM_BINARY;
+// #[cfg(feature = "wasm-binary")]
+// #[cfg(not(target_arch = "wasm32"))]
+// pub use code::WASM_BINARY_OPT as WASM_BINARY;
 
-#[cfg(feature = "wasm-binary")]
-#[cfg(not(target_arch = "wasm32"))]
+// #[cfg(feature = "wasm-binary")]
+// #[cfg(not(target_arch = "wasm32"))]
+// mod code {
+//     include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+// }
+
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "std")]
 mod code {
     include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 }
+
+#[cfg(feature = "std")]
+pub use code::WASM_BINARY_OPT as WASM_BINARY;
+
+#[cfg(not(feature = "std"))]
+pub const WASM_BINARY: &[u8] = &[];
+
+#[cfg(not(feature = "std"))]
+pub mod wasm;
